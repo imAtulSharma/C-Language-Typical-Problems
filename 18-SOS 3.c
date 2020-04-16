7f9a6aac46e2dcc1c8f6beea480398a0093b16a1
Atul Sharma <atulsharma20may@gmail.com>
2020-04-16 17:32:57 +0530

    To upload files

---
+++ 18-SOS 3.c
@@ -0,0 +1,20 @@
+//Summation of series 1/2 + 2/3 + 3/4 + 4/5 + 5/6 + ...... + n/n+1 upto n terms
+
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+    int n, i = 1;
+    float sum = 0;
+
+    printf("Enter the number of terms...\n");
+    scanf("%d", &n);
+
+    while(i <= n)
+    {
+        sum += (float)i/(i+1);
+        i++;
+    }
+    printf("The summation of series 1/2 + 2/3 + 3/4 + 4/5 + 5/6 + ...... + %d/%d is %.2f", n, n+1, sum);
+}
