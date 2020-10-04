bf40bd07b841290e533320d8a385e8c4a36497f2
YASH <72142451+yashcoder-1@users.noreply.github.com>
2020-10-04 11:09:04 +0530

    Update 02-Sorting array in ascening order.c

--- 02-Sorting array in ascening order.c
+++ 02-Sorting array in ascening order.c
@@ -1,10 +1,18 @@
 //Programme to sort arrray in ascending order
 
+  /*
+     * C program to accept N numbers and arrange them in an ascending order
+     */
+
 #include<stdio.h>
+
+/* header file is included. */
 
 void main()
 {   
     int Size_of_array;
+    
+    */ we can also used this as int A[10] this means we have declared the array of integer type and whose size is 10,and the name of the array is A */
 
     printf("Enter the size of array : ");
     scanf("%d", &Size_of_array);
@@ -38,6 +46,8 @@ void main()
     }
 
     printf("\b\b}\nThe sorted Array is in ascending order...\n{");
+    
+    */ when displaying \b would move the cursor one step back, this is why we have used /b here just for formating purposes .*/
     for (int i = 0; i < Size_of_array; i++)
     {
         printf("%d, ", array[i]);
